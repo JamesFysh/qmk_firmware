a07a274c8622103aca5399e3fbb198146efe72fd
James Fysh <james.fysh@gmail.com>
2019-05-25 13:19:40 +1000

    better tapping term value

--- keyboards/keebio/iris/keymaps/jamesfysh/config.h
+++ keyboards/keebio/iris/keymaps/jamesfysh/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 /* Mod-Tap configuration */
-#define TAPPING_TERM              300
+#define TAPPING_TERM              200
 #define IGNORE_MOD_TAP_INTERRUPT
 
 /* Mousekey configuration */
