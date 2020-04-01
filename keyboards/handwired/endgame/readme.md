f789f89e36e4935518e47678ed945af447027da3
James Fysh <james.fysh@gmail.com>
2020-04-01 11:30:52 +1100

    correct rules, store away how to program the MCU

---
+++ keyboards/handwired/endgame/readme.md
@@ -0,0 +1,2 @@
+How to program STM32F303 (QMK Proton C)
+dfu-util -a0 -D handwired_endgame_jamesfysh.bin -s0x8000000:leave -R
--- keyboards/handwired/endgame/rules.mk
+++ keyboards/handwired/endgame/rules.mk
@@ -1,9 +1,4 @@
 # Using the QMK Proton
 MCU = STM32F303
 
-
-CONSOLE_ENABLE = yes
 EXTRAKEY_ENABLE = yes # Audio control and System control
-
-RGB_LIGHT_ENABLE = WS2812
-WS2812_DRIVER = bitbang
